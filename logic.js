d88a4fe9fff45c5bb1882c7f60ad46c2e52c8506
NickForma <nickforma60@gmail.com>
2020-10-06 21:11:56 -0400

    styling checkboxes

--- logic.js
+++ logic.js
@@ -15,26 +15,32 @@ const writeQuestions = function (data) {
     console.log(e);
     document.getElementById("questions").innerHTML += `
 
-    <form>
-    <p><em>Question ${a + 1}: </em>${e.question}</p>
+    <div class="box">
+      <form>
+        <p class="question"><em>Question ${a + 1}: </em>${e.question}</p>
     
-    <input type="radio" name="incorrect_answer">${e.incorrect_answers[0]}</input>
-    <input type="radio" name="incorrect_answer">${e.incorrect_answers[1]}</input>
-    <input type="radio" name="incorrect_answer">${e.incorrect_answers[2]}</input>
-    <input type="radio" name="incorrect_answer">${e.incorrect_answers[3]}</input>
+        <input type="radio" name="incorrect_answer" "id="answers">${e.incorrect_answers[0]}</input>
+        <input type="radio" name="incorrect_answer" "id="answers">${e.incorrect_answers[1]}</input>
+        <input type="radio" name="incorrect_answer" "id="answers">${e.incorrect_answers[2]}</input>
+        <input type="radio" name="incorrect_answer" "id="answers">${e.correct_answer}</input>
 
-    </form>
-      
+                <div class="input-group">
+          <div class="input-group-prepend">
+            <div class="input-group-text">
+              <input type="radio" aria-label="Radio button for following text input">${e.incorrect_answers[0]}
+              <input type="radio" aria-label="Radio button for following text input">${e.incorrect_answers[1]}
+              <input type="radio" aria-label="Radio button for following text input">${e.incorrect_answers[2]}
+              <input type="radio" aria-label="Radio button for following text input">${e.correct_answer}
+            </div>
+          </div>
+        </div>
 
 
         
+      </form> 
            
-    
-
-
-
         <p>${e.correct_answer}</p>
-        
+    </div>
         `;
     a++;
   });
